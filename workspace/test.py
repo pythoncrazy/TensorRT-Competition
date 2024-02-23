caecf64ae255858c7566415fd549bd1c0ab60b67
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2024-02-23 07:40:00 +0000

    Thursday, February 22, 2024 at 11:40 PM Pacific Standard Time

--- workspace/test.py
+++ workspace/test.py
@@ -1,27 +1,22 @@
-from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
-import torch 
+import torch
+from diffusers import DDIMScheduler
+from diffusers.pipelines.stable_diffusion import StableDiffusionPipeline
 
-model_id = "stabilityai/stable-diffusion-2"
+# Use the DDIMScheduler scheduler here instead
+scheduler = DDIMScheduler.from_pretrained("stabilityai/stable-diffusion-2-1",
+                                            subfolder="scheduler")
 
-pipe = StableDiffusionPipeline.from_pretrained(
-    model_id,
-    custom_pipeline="stable_diffusion_tensorrt_txt2img",
-    # revision="fp16",
-    torch_dtype=torch.float16,
-    scheduler=scheduler,
-)
+pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1",
+                                                custom_pipeline="stable_diffusion_tensorrt_txt2img",
+                                                revision='fp16',
+                                                torch_dtype=torch.float16,
+                                                scheduler=scheduler,)
 
 # re-use cached folder to save ONNX models and TensorRT Engines
-cache_dir = os.path.basename(model_id) + '_cached_trt'
-os.makedirs(cache_dir, exist_ok=True)
-print(f'cached dir: {cache_dir}')
-pipe.set_cached_folder(
-    cache_dir,
-    revision="fp16",
-)
+pipe.set_cached_folder("stabilityai/stable-diffusion-2-1", revision='fp16',)
 
 pipe = pipe.to("cuda")
 
 prompt = "a beautiful photograph of Mt. Fuji during cherry blossom"
 image = pipe(prompt).images[0]
-image.save("tensorrt_mt_fuji.png")
+image.save('tensorrt_mt_fuji.png')
